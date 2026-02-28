e14267b5148e92ca1e2184446a6c0231e122096f
Cedric F <cedric.fontaine@gmx.fr>
2026-02-28 03:58:59 +0100

    add middleware for CORS, security, and logging

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const puppeteer = require('puppeteer');
 
 const app = express();
+app.use(cors());
+app.use(helmet());
+app.use(morgan('combined'));
 app.use(express.json({ limit: '5mb' }));
 
 app.get('/', (req, res) => {
